d24f64281bee3ced5f4d01a40b0510b79a4a539e
Alex <Alex@mail.ru>
2018-05-31 21:23:26 +0400

    --

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #mkdir -p home/box/web/{public/{img,css,js},uploads,etc}
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+sudo ln -sf ~/Stepic/Stepic_Web_Technologies/home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx start
